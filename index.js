fc865e2a554d53fec8cd412d3a5a5158d2a38c3e
Rahul Parui <paruirahul800@gmail.com>
2026-03-19 22:06:22 +0530

    backend

---
+++ index.js
@@ -0,0 +1,9 @@
+const a=require("figlet");
+a("RAHUL", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
